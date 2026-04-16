5818f5b8876f23ca947df86b97d07ded2e2ef1fd
FoolAntony <mrcrazytony@gmail.com>
2026-04-16 12:50:31 +0200

    Add links to message, show only news in EN language, send only 10 news, added TOPIC variable used to define the required news topic

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 from send_email import send_email
 
+TOPIC = "tesla"
+
 api_key = "YOUR_API_KEY"
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-       "2026-03-16&sortBy=publishedAt" \
-       f"&apiKey={api_key}"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={TOPIC}" \
+      "&from=2026-03-16" \
+      "&sortBy=publishedAt" \
+      f"&apiKey={api_key}" \
+      "&language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,9 +19,13 @@ content =  request.json()
 
 # Access the article titles and descriptions
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:10]:
     if article["title"] and article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+        body =  "Subject: Today's News" + "\n" \
+                + body + article["title"] + "\n" \
+                + article["description"] + "\n" \
+                + article["url"] \
+                + 2 * "\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
